639e95d2cc4cc989858e4c93e4093919c093487d
densmyslov <denis@bergena.io>
2025-04-28 12:11:00 +0200

    fixed bug

--- stripe_payment.py
+++ stripe_payment.py
@@ -29,7 +29,7 @@ def create_checkout_session():
                 },
             ],
             mode='payment',
-            success_url=SUCCESS_URL
+            success_url=SUCCESS_URL,
             cancel_url=CANCEL_URL,
             automatic_tax={'enabled': False},
             metadata={
